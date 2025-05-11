946bc50fb54c442b1a5b2d938ec8fe84b8939a8e
jmcrp <yanismassot2006@gmail.com>
2025-05-11 23:20:14 +0200

    ADD : Shield

---
+++ Saly/Assets/Scripts/Shield.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ForceField : MonoBehaviour
+{
+    bool IsActive = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        IsActive = true; // TO REMOVE
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
