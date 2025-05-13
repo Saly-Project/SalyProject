2807f1387403c9e207074a05b23d3094d433c205
jmcrp <yanismassot2006@gmail.com>
2025-05-13 13:32:39 +0200

    UPDATE : Shield

--- Saly/Assets/Scripts/Shield.cs
+++ Saly/Assets/Scripts/Shield.cs
@@ -4,15 +4,21 @@ public class ForceField : MonoBehaviour
 {
     bool IsActive = false;
 
+    public GameObject Shield;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         IsActive = true; // TO REMOVE
+        GameObject.Instantiate(Shield);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Shield.SetActive(IsActive);
+        Shield.transform.position = this.transform.position;
+        if (Input.GetKeyDown(KeyCode.E))
+            IsActive = !IsActive;
     }
 }
