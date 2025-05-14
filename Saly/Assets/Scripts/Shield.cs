7069abf00207503de136f715c4c8bd477c0288da
jmcrp <yanismassot2006@gmail.com>
2025-05-14 12:41:09 +0200

    UPDATE : Shield, nbCollisions + Timer

--- Saly/Assets/Scripts/Shield.cs
+++ Saly/Assets/Scripts/Shield.cs
@@ -4,11 +4,14 @@ public class ForceField : MonoBehaviour
 {
     public bool Charged;
     public float Duration;
+    public int MaxCollisions;
     public GameObject Shield;
     public GameObject UIshield;
+    public GameObject RechargeVFX;
 
     bool IsActive = false;
     float ShieldChrono = 0;
+    int Collisions = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,31 +22,40 @@ public class ForceField : MonoBehaviour
 
     void ActivateShield()
     {
-        if (Charged)
+        if (Charged) // if the skill can be enabled
         {
-            Shield.SetActive(true);
             IsActive = true;
-            ShieldChrono = 0;
             Charged = false;
+            ShieldChrono = Duration;
+            Collisions = 0;
+            Shield.SetActive(true);
             UIshield.SetActive(false);
         }
+    }
+
+    void DesactivateShield()
+    {
+        IsActive = false;
+        Shield.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Shield.transform.position = this.transform.position;
+        Shield.transform.position = this.transform.position; // the shield follows the player's position
 
         if (Input.GetKeyDown(KeyCode.E))
+        {
             ActivateShield();
+        }
 
         if (IsActive)
         {
-            ShieldChrono += Time.deltaTime;
-            if (ShieldChrono >= Duration)
+            ShieldChrono -= Time.deltaTime;
+
+            if (ShieldChrono <= 0)
             {
-                IsActive = false;
-                Shield.SetActive(false);
+                DesactivateShield();
             }
         }
     }
@@ -55,9 +67,18 @@ public class ForceField : MonoBehaviour
             if (!Charged)
             {
                 Charged = true;
-                other.gameObject.SetActive(false);
                 Destroy(other.gameObject);
                 UIshield.SetActive(true);
+                var recharge = Instantiate(RechargeVFX, other.transform.position, Quaternion.identity) as GameObject;
+                Destroy(recharge, 2f);
+            }
+        }
+        else
+        {
+            Collisions++;
+            if (Collisions >= MaxCollisions)
+            {
+                DesactivateShield();
             }
         }
     }
