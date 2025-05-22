09d4d7b9024de18f22e03fc37ac5748da93ba1c5
levpastel <lev.pastel@epita.fr>
2025-05-22 13:42:21 +0200

    ADD:
    Some sound effects

--- Saly/Assets/Scripts/Shooting.cs
+++ Saly/Assets/Scripts/Shooting.cs
@@ -7,6 +7,7 @@ public class WeaponShooting : NetworkBehaviour
 
     public Camera cam;
     [SerializeField] private GameObject projectile;
+    [SerializeField] private AudioClip projectileFX;
     public Transform LeftFirePoint;
     public Transform RightFirePoint;
     private Vector3 destination;
@@ -33,35 +34,40 @@ public class WeaponShooting : NetworkBehaviour
 
         if (!PauseMenu.isPaused && Input.GetButton("Fire") && Time.time >= timeToFire){
             timeToFire = Time.time + 1/fireRate;
+            AudioSource.PlayClipAtPoint(projectileFX, transform.position, 0.5f);
             Fire();
         }
     }
 
 
 
-    public void Fire(){
+    public void Fire()
+    {
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         ShootServerRpc();
 
 
-        if (!Physics.Raycast(ray, out RaycastHit hit, maxRange, weaponHitLayers)){
+        if (!Physics.Raycast(ray, out RaycastHit hit, maxRange, weaponHitLayers))
+        {
             destination = ray.GetPoint(1000);
             return;
         }
-        
+
         destination = hit.point;
-        
+
         // damage relative to distance
         float distance = Vector3.Distance(transform.position, hit.point);
         float normalizedDistance = Mathf.Clamp01((distance - 7.5f) / (maxRange - 7.5f));
         damage = (int)Mathf.Lerp(10, 1, normalizedDistance);
 
-        
+
         if (hit.transform.TryGetComponent(out PlayerHealth health))
         {
             health.TakeDamageServerRpc(damage);
             Debug.Log(damage + "damages inflicted");
         }
+        
+
     }
 
 
